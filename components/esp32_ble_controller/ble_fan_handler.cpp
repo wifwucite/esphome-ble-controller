7caa9fd0014026616e68daf3c0a6bafb619402b5
wifwucite <sv.thiel@web.de>
2021-06-10 19:09:37 +0200

    use constants

--- components/esp32_ble_controller/ble_fan_handler.cpp
+++ components/esp32_ble_controller/ble_fan_handler.cpp
@@ -9,11 +9,20 @@ namespace esp32_ble_controller {
 
 static const char *TAG = "ble_fan_handler";
 
+static const char *OPT_FAN_ON = "on";
+static const char *OPT_FAN_OFF = "off";
+
+static const char *OPT_OSCILLATING_YES = "yes";
+static const char *OPT_OSCILLATING_NO = "no";
+
+static const char *OPT_DIRECTION_FWD = "forward";
+static const char *OPT_DIRECTION_REV = "reverse";
+
 void BLEFanHandler::send_value(bool on_off) {
   string state_as_string;
 
   state_as_string = "fan=";
-  state_as_string += on_off ? "on" : "off";
+  state_as_string += on_off ? OPT_FAN_ON : OPT_FAN_OFF;
 
   const FanState* fan_state = get_component();
   const auto& traits = fan_state->get_traits();
@@ -30,12 +39,12 @@ void BLEFanHandler::send_value(bool on_off) {
   
   if (traits.supports_oscillation()) {
     state_as_string += " oscillating=";
-    state_as_string += fan_state->oscillating ? "yes" : "no";
+    state_as_string += fan_state->oscillating ? OPT_OSCILLATING_YES : OPT_OSCILLATING_NO;
   }
   
   if (traits.supports_direction()) {
     state_as_string += " direction=";
-    state_as_string += fan_state->direction == fan::FAN_DIRECTION_FORWARD ? "forward" : "reverse";
+    state_as_string += fan_state->direction == fan::FAN_DIRECTION_FORWARD ? OPT_DIRECTION_FWD : OPT_DIRECTION_REV;
   }
 
   BLEComponentHandlerBase::send_value(state_as_string);
@@ -60,11 +69,11 @@ void BLEFanHandler::on_characteristic_written() {
   auto call = fan_state->make_call();
   const auto& traits = fan_state->get_traits();
   for (const string& option : split(value)) {
-    if (option == "on") {
+    if (option == OPT_FAN_ON) {
       call.set_state(true);
       continue;
     }
-    if (option == "off") {
+    if (option == OPT_FAN_OFF) {
       call.set_state(false);
       continue;
     }
@@ -81,22 +90,22 @@ void BLEFanHandler::on_characteristic_written() {
     }
 
     if (traits.supports_oscillation()) {
-      if (option == "yes") {
+      if (option == OPT_OSCILLATING_YES) {
         call.set_oscillating(true);
         continue;
       }
-      if (option == "no") {
+      if (option == OPT_OSCILLATING_NO) {
         call.set_oscillating(false);
         continue;
       }
     }
     
     if (traits.supports_direction()) {
-      if (option == "forward") {
+      if (option == OPT_DIRECTION_FWD) {
         call.set_direction(fan::FAN_DIRECTION_FORWARD);
         continue;
       } 
-      if (option == "reverse") {
+      if (option == OPT_DIRECTION_REV) {
         call.set_direction(fan::FAN_DIRECTION_REVERSE);
         continue;
       }
